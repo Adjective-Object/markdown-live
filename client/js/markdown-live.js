7679c1c2ba0f07b25ab497b18fe3e9ecb25cc89e
Daan van Ham <daan@vanham.io>
2016-09-27 22:24:53 +0200

    fixed indentation

--- client/js/markdown-live.js
+++ client/js/markdown-live.js
@@ -229,12 +229,12 @@ class FilesController extends Framework {
       this.element.documents.appendChild(newFrame);
     }
     else {
-        Array.prototype.forEach.call(
-            this.element.documents.children,
-            function(item) {
-                item.remove();
-            }
-		);
+      Array.prototype.forEach.call(
+        this.element.documents.children,
+        function(item) {
+          item.remove();
+        }
+      );
     }
   }
 
