82276a04f2ae8467d39bec11496a52da387474bb
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2016-10-09 17:13:26 +0200

    break toasts model into own file, add type annotation

---
+++ client/js/toasts.js
@@ -0,0 +1,108 @@
+// @flow
+import types from 'flowtype/sangria-types';
+
+import Framework from './Framework';
+import {network} from './platform';
+import notificationTemplate from './templates/notification.handlebars';
+import _ from 'underscore';
+
+function htmlToDomNodes(html: string): HTMLElement {
+  const template: HTMLElement = // HTMLTemplateElement
+    document.createElement('template');
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+export default class Toasts extends Framework {
+  elements: {
+    dock: HTMLElement
+  };
+
+  initialize() {
+    this.elements = {
+      dock: document.getElementById('notification-dock'),
+    };
+  }
+
+  events() {
+    network.on('toast', (msg: types.Toast) => {
+      this.notify(
+        msg.title,
+        msg.text || '',
+        msg.kind || 'info',
+        msg.actions || [],
+        msg.timeout || null);
+    });
+  }
+
+  notify(
+    title: string,
+    text: string,
+    kind: types.ToastKind = 'info',
+    actions: types.ToastMessage[] = [],
+    timeout: number = 0) {
+    let id: ?number = null;
+
+    actions = actions.map(
+      (a: types.ToastMessage): types.EvaluatedToastMessage => {
+        if (typeof a.action === 'string') {
+          return {
+            text: a.text,
+            /* eslint-disable no-eval */
+            action: eval('(function() {' + a.action + '})'),
+            /* eslint-enable no-eval */
+          };
+        }
+
+        return a;
+      });
+
+    actions.push({
+      text: 'ok',
+      action: (e: MouseEvent): void => this.dismiss(id),
+    });
+
+    // create the element, bind the actions
+    const toast = htmlToDomNodes(notificationTemplate({
+      title, text, kind, actions,
+    }));
+
+    _.each(
+        toast.querySelectorAll('button[notification-action]'),
+        (button: HTMLElement, i: number) => { // HTMLButtonElement
+          button.addEventListener('click', actions[i].action);
+        });
+
+    id = this.push({
+      text,
+      kind,
+      actions,
+      timeoutHandle: timeout
+        ? setTimeout(() => { this.dismiss(id); }, timeout)
+        : null,
+      element: toast,
+    });
+
+    this.elements.dock.appendChild(toast);
+    toast.classList.add('enter');
+    setTimeout(() => {
+      toast.classList.remove('enter');
+    }, 0);
+  }
+
+  dismiss(id: ?number) {
+    if (id === null || id === undefined) return;
+
+    const tokill = this.data[id];
+    if (tokill.timeoutHandle) {
+      clearTimeout(tokill.timeoutHandle);
+    }
+
+    this.rm(id);
+
+    tokill.element.classList.add('exit');
+    setTimeout(() => {
+      tokill.element.remove();
+    }, 200);
+  }
+}
